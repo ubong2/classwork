1a9c95351002d5e660673d6030ac891dc8f25a18
ubong2 <ubonguboh2@gmail.com>
2025-05-05 10:36:11 +0100

    My first commit

---
+++ calculator.py
@@ -0,0 +1,20 @@
+def addition(a,b):
+   return a+b
+   
+def sub(a,b):
+    return a-b
+
+def max(a,b):
+    if(b>a):
+       return b
+    else:
+        return a
+
+def percent(a,v):
+    return a*v/100   
+
+x = 7 
+  
+y = "hello, welcome" 
+
+z = ["hey", 4, 6.7 ,"dan"]   
